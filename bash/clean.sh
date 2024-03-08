29b95a404d816c8207208b474609fe512730328a
Nekoler <2697446335@qq.com>
2024-03-08 16:47:00 +0800

    fix

--- bash/clean.sh
+++ bash/clean.sh
@@ -1,4 +1,4 @@
 apt-get clean
 apt-get autoremove
 dpkg -l |grep ^rc|awk '{print $2}' |xargs apt purge -y
-rm -rf ~/.bash_history ~/.python_history ~/.cache ~/.viminfo ~/.config/BaiduPCS-Go/pcs_command_history.txt ~/.npm/
+rm -rf ~/.bash_history ~/.python_history ~/.cache ~/.viminfo ~/.config/BaiduPCS-Go/pcs_command_history.txt ~/.npm/ ~/.wget-hsts
